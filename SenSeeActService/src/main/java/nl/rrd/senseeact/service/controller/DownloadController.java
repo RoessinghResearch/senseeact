5e63d4c2f484a5eebfa068b849be5985d6053428
Dennis Hofs <d.hofs@rrd.nl>
2023-11-21 16:55:12 +0100

    Updated DownloadController

--- SenSeeActService/src/main/java/nl/rrd/senseeact/service/controller/DownloadController.java
+++ SenSeeActService/src/main/java/nl/rrd/senseeact/service/controller/DownloadController.java
@@ -7,12 +7,18 @@ import nl.rrd.senseeact.client.model.Project;
 import nl.rrd.senseeact.client.project.BaseProject;
 import nl.rrd.senseeact.client.project.ProjectRepository;
 import nl.rrd.senseeact.dao.Database;
+import nl.rrd.senseeact.dao.DatabaseCriteria;
+import nl.rrd.senseeact.dao.DatabaseSort;
 import nl.rrd.senseeact.service.ProtocolVersion;
 import nl.rrd.senseeact.service.QueryRunner;
 import nl.rrd.senseeact.service.exception.HttpException;
 import nl.rrd.senseeact.service.export.DataExporterFactory;
+import nl.rrd.senseeact.service.export.DataExporterManager;
+import nl.rrd.senseeact.service.model.DataExportRecord;
+import nl.rrd.senseeact.service.model.DataExportTable;
 import nl.rrd.senseeact.service.model.User;
 import nl.rrd.utils.AppComponents;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,6 +30,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/v{version}/download")
 public class DownloadController {
+	@Autowired
+	private DataExporterManager exporterManager;
+
 	@RequestMapping(value="/projects", method=RequestMethod.GET)
 	public List<Project> getProjects(
 			HttpServletRequest request,
@@ -32,6 +41,17 @@ public class DownloadController {
 			@Parameter(hidden = true)
 			String versionName) throws HttpException, Exception {
 		return QueryRunner.runAuthQuery(this::doGetProjects, versionName,
+				request, response);
+	}
+
+	@RequestMapping(value="/list", method=RequestMethod.GET)
+	public List<DataExportRecord> getDownloadList(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			@PathVariable("version")
+			@Parameter(hidden = true)
+			String versionName) throws HttpException, Exception {
+		return QueryRunner.runAuthQuery(this::doGetDownloadList, versionName,
 				request, response);
 	}
 
@@ -51,5 +71,16 @@ public class DownloadController {
 			result.add(new Project(code, project.getName()));
 		}
 		return result;
+	}
+
+	private List<DataExportRecord> doGetDownloadList(ProtocolVersion version,
+			Database authDb, User user) throws HttpException, Exception {
+		DataExportTable table = new DataExportTable();
+		DatabaseCriteria criteria = new DatabaseCriteria.Equal(
+				"user", user.getUserid());
+		DatabaseSort[] sort = new DatabaseSort[] {
+			new DatabaseSort("utcTime", true)
+		};
+		return authDb.select(table, criteria, 0, sort);
 	}
 }
--- SenSeeActService/src/main/java/nl/rrd/senseeact/service/export/DataExporterManager.java
+++ SenSeeActService/src/main/java/nl/rrd/senseeact/service/export/DataExporterManager.java
@@ -1,8 +1,21 @@
 package nl.rrd.senseeact.service.export;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class DataExporterManager {
 	private List<DataExporter> exporters = new ArrayList<>();
+
+	@PostConstruct
+	public void init() {
+	}
+
+	@PreDestroy
+	public void destroy() {
+	}
 }
