ea3c556a5bc5299020def9be14e7f8ede2bb6d7d
Dennis Hofs <d.hofs@rrd.nl>
2024-04-22 11:43:46 +0200

    Updated QuestionnaireRecord

--- SenSeeActClient/src/main/java/nl/rrd/senseeact/client/model/questionnaire/QuestionnaireRecord.java
+++ SenSeeActClient/src/main/java/nl/rrd/senseeact/client/model/questionnaire/QuestionnaireRecord.java
@@ -3,27 +3,63 @@ package nl.rrd.senseeact.client.model.questionnaire;
 import nl.rrd.senseeact.dao.AbstractDatabaseObject;
 import nl.rrd.senseeact.dao.DatabaseField;
 import nl.rrd.senseeact.dao.DatabaseType;
+import nl.rrd.utils.exception.ParseException;
+import nl.rrd.utils.json.JsonMapper;
 
 public class QuestionnaireRecord extends AbstractDatabaseObject {
-	@DatabaseField(value=DatabaseType.STRING)
+	@DatabaseField(value=DatabaseType.STRING, index=true)
 	private String name;
 
-	@DatabaseField(value=DatabaseType.TEXT)
-	private String questionnaireXml;
+	@DatabaseField(value=DatabaseType.TEXT, json=true)
+	private String questionnaire;
+	private Questionnaire questionnaireObject;
 
+	/**
+	 * Returns the questionnaire name.
+	 *
+	 * @return the questionnaire name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the questionnaire name.
+	 *
+	 * @param name the questionnaire name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getQuestionnaireXml() {
-		return questionnaireXml;
+	/**
+	 * Returns the JSON code for the questionnaire. This method is used for the
+	 * DAO. Users can call {@link #getQuestionnaireObject()
+	 * getQuestionnaireObject()}.
+	 *
+	 * @return the JSON code for the questionnaire
+	 */
+	public String getQuestionnaire() {
+		return JsonMapper.generate(questionnaireObject);
 	}
 
-	public void setQuestionnaireXml(String questionnaireXml) {
-		this.questionnaireXml = questionnaireXml;
+	/**
+	 * Sets the JSON code for the questionnaire. This method is used for the
+	 * DAO. Users can call {@link #setQuestionnaireObject(Questionnaire)
+	 * setQuestionnaireObject()}.
+	 *
+	 * @param questionnaire the JSON code for the questionnaire
+	 */
+	public void setQuestionnaire(String questionnaire) throws ParseException {
+		questionnaireObject = JsonMapper.parse(questionnaire,
+				Questionnaire.class);
+	}
+
+	public Questionnaire getQuestionnaireObject() {
+		return questionnaireObject;
+	}
+
+	public void setQuestionnaireObject(Questionnaire questionnaireObject) {
+		this.questionnaireObject = questionnaireObject;
 	}
 }
