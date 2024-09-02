05b3ab23fba52295d76d8ecd9349313352d931b7
Dennis Hofs <d.hofs@rrd.nl>
2024-09-02 13:57:53 +0200

    Updated MfaRecord

--- SenSeeActService/src/main/java/nl/rrd/senseeact/service/model/MfaRecord.java
+++ SenSeeActService/src/main/java/nl/rrd/senseeact/service/model/MfaRecord.java
@@ -1,6 +1,5 @@
 package nl.rrd.senseeact.service.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
