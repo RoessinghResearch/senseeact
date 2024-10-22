167884a5f230eb00d3bf48cde6fe222e579455fb
Dennis Hofs <d.hofs@rrd.nl>
2024-10-22 13:19:23 +0200

    Updated MFA

--- SenSeeActService/src/main/web/js/pages/me/account-mfa.js
+++ SenSeeActService/src/main/web/js/pages/me/account-mfa.js
@@ -1,8 +1,22 @@
 class MyAccountMfaForm {
 	constructor() {
+		var self = this;
 		$('#mfa-title').text(i18next.t('mfa_title'));
 		$('#mfa-intro').text(i18next.t('mfa_intro'));
 		let button = $('#mfa-add-button');
 		button.text(i18next.t('enable'));
+		animator.addAnimatedClickHandler(button, button,
+			'animate-blue-button-click',
+			null,
+			(result) => {
+				self._onAddClick();
+			}
+		);
+	}
+
+	_onAddClick() {
+		let dlg = Dialogue.openDialogue();
+		dlg.initMainForm(i18next.t('mfa_title'));
+		dlg.centralButtons.addCancelButton();
 	}
 }
